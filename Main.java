d9b1657a18832be8a30329596746382962c5f823
Davidkaka123 <1721776927@qq.com>
2016-07-28 23:58:58 +0800

    20160728(3)

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 /**
- * Created by David on 2016/7/27.
+ * Created by David on 2016/7/28.
  */
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
     }
 }
