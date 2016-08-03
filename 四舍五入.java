c58a8d966e8fe73840b15f5666c33b5adf2e72c8
Davidkaka123 <1721776927@qq.com>
2016-08-03 21:37:47 +0800

    0803

---
+++ 四舍五入.java
@@ -0,0 +1,14 @@
+//import java.util.Scanner;
+//
+///**
+// * Created by davidshuvli on 2016/8/3.
+// */
+//public class ËÄÉáÎåÈë {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        while (sc.hasNextFloat()) {
+//            float input = sc.nextFloat();
+//            System.out.println(Math.round(input));
+//        }
+//    }
+//}
