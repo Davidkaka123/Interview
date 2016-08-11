e47e78dfcb0a287741f90333802e3b5d25add0cd
Davidkaka123 <1721776927@qq.com>
2016-08-11 21:44:36 +0800

    0811

---
+++ 不用加号计算加法.java
@@ -0,0 +1,20 @@
+///**
+// * Created by davidshuvli on 2016/8/11.
+// */
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.print(Add(1, 2));
+//    }
+//
+//    public static int Add(int num1,int num2) {
+//        int sum = 0, carry = 0;
+//
+//        while (num2 != 0) {
+//            sum = num1 ^ num2;
+//            carry = (num1 & num2) << 1;
+//            num1 = sum;
+//            num2 = carry;
+//        }
+//        return num1;
+//    }
+//}
