d1fa46ea4087f477a3f6f5ef2a74ff656170228f
Davidkaka123 <1721776927@qq.com>
2016-08-11 21:45:04 +0800

    0811

---
+++ 不用循环计算1到n.java
@@ -0,0 +1,15 @@
+///**
+// * Created by davidshuvli on 2016/8/11.
+// */
+//public class Main {
+//    public static void main(String[] args) {
+//        int n = 5;
+//        System.out.println(Sum_Solution(n));
+//    }
+//
+//    public static int Sum_Solution(int n) {
+//        int sum = n;
+//        boolean judge = (n > 0) && (sum += Sum_Solution(n - 1)) > 0;
+//        return sum;
+//    }
+//}
