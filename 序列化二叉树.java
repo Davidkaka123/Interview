6d3d10c79e8c1e9f566f465b8f02ca220213061d
Davidkaka123 <1721776927@qq.com>
2016-08-26 22:49:09 +0800

    0826

---
+++ 序列化二叉树.java
@@ -0,0 +1,51 @@
+/**
+ * Created by davidshuvli on 2016/8/26.
+ */
+
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int index = -1;
+    String Serialize(TreeNode root) {
+        StringBuffer sb = new StringBuffer();
+        if(root == null){
+            sb.append("#,");
+            return sb.toString();
+        }
+        sb.append(root.val + ",");
+        sb.append(Serialize(root.left));
+        sb.append(Serialize(root.right));
+        return sb.toString();
+    }
+    TreeNode Deserialize(String str) {
+        index++;
+        int len = str.length();
+        if(index >= len){
+            return null;
+        }
+        String[] strr = str.split(",");
+        TreeNode node = null;
+        if(!strr[index].equals("#")){
+            node = new TreeNode(Integer.valueOf(strr[index]));
+            node.left = Deserialize(str);
+            node.right = Deserialize(str);
+        }
+
+        return node;
+    }
+}
